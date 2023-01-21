2571e9aa6fa1ffa0a3dce125c9ac92f177d4cfe4
ferz1ka <fhozanzoti@hotmail.com>
2023-01-21 15:41:31 -0300

    server, web and mobile initial config

---
+++ server/src/server.ts
@@ -0,0 +1,21 @@
+import Fastify from "fastify";
+import cors from "@fastify/cors"
+import { PrismaClient } from '@prisma/client'
+
+const app = Fastify()
+const prisma = new PrismaClient()
+
+app.register(cors, {
+  origin: 'http://localhost:3000'
+})
+
+app.get('/', async () => {
+  const habits = await prisma.habit.findMany()
+  return habits
+})
+
+app.listen({
+  port: 3333,
+}, () => {
+  console.log('Server running on 3333..')
+})
